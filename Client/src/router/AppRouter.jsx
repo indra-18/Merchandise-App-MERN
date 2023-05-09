9752056646b066ce7f6b26cf7bdaa1d5c324a475
indra-18 <indraind0918@gmail.com>
2023-05-09 17:45:43 +0530

    login page created

--- Client/src/router/AppRouter.jsx
+++ Client/src/router/AppRouter.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/HomePage/Home";
 import Shop from "../components/ShopPage/Shop";
 import Details from "../components/Details";
+import LoginPage from "../components/LoginPage/LoginPage";
 
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/details/:id" element={<Details />} />
       </Routes>
