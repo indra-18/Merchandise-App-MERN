0b87043d6aa92ddfe84592151b6049c5e9783576
indra-18 <indraind0918@gmail.com>
2023-05-17 07:07:43 +0530

    added necessary functionality for creating payment Intent

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,10 @@ const app = express();
 const mongoose = require('mongoose')
 const productsRouter = require('./routes/product.routes')
 const usersRouter = require('./routes/user.routes')
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+app.use(express.static("public"));
+app.use(express.json());
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -20,6 +23,21 @@ app.use('/', usersRouter)
 app.get('/', (req, res) => {
     res.send('Hello From Frenzy Backend Application')
 })
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { totalPrice } = req.body;
+    console.log(totalPrice)
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: totalPrice,
+      currency: "inr",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  });
 
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => {
